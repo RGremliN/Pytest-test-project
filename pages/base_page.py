8dade94c2b3648ba65ec141771fcb54802fca8a7
Roman Dolgiy <zendesar@gmail.com>
2020-04-09 08:39:57 +0300

    Add class BasePage

---
+++ pages/base_page.py
@@ -0,0 +1,20 @@
+class BasePage():
+	#конструктор — метод, который вызывается, когда мы создаем объект
+	def __init__(self, browser, url):
+    self.browser = browser
+    self.url = url
+
+    #открыть нужную страницу в браузере, используя метод get()
+    def open(self):
+    	self.browser.get(self.url)
+
+
+
+
+
+
+
+
+
+
+
