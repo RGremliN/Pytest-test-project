eb6ce7a8cc96d686dc16025f6b95bb86449794c5
Roman Dolgiy <zendesar@gmail.com>
2020-04-09 09:32:15 +0300

    Add MainPage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,8 @@
 class BasePage():
 	#конструктор — метод, который вызывается, когда мы создаем объект
 	def __init__(self, browser, url):
-    self.browser = browser
-    self.url = url
+	    self.browser = browser
+	    self.url = url
 
     #открыть нужную страницу в браузере, используя метод get()
     def open(self):
