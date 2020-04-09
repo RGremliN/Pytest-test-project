4dc84e0c5c53334e389d2c5714dfacebd1662b7f
Roman Dolgiy <zendesar@gmail.com>
2020-04-09 09:47:27 +0300

    Rewrite test for main page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,12 @@
 class BasePage():
-	#конструктор — метод, который вызывается, когда мы создаем объект
-	def __init__(self, browser, url):
-	    self.browser = browser
-	    self.url = url
+    #конструктор — метод, который вызывается, когда мы создаем объект
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
 
     #открыть нужную страницу в браузере, используя метод get()
     def open(self):
-    	self.browser.get(self.url)
+        self.browser.get(self.url)
 
 
 
